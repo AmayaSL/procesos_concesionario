c4525afc819c3d5543be871869ecc53d44cd5e6a
Sebastian Amaya <samayal@ufpso.edu.co>
2023-03-16 20:53:48 -0500

    Creacion de servicios y endpoint de crear un usuario

---
+++ src/main/java/com/procesos/concesionario/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.procesos.concesionario.controllers;
+
+import com.procesos.concesionario.models.User;
+import com.procesos.concesionario.services.UserService;
+import com.procesos.concesionario.services.UserServiceImp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserServiceImp userServiceImp;
+    @GetMapping(value = "/user/{id}")
+    public User getById(@PathVariable(name = "id") Long id){
+
+        return userServiceImp.getUserById(id);
+    }
+
+    @PostMapping(value = "/user")
+    public User createUser(@RequestBody User user){
+        return userServiceImp.createUser(user);
+    }
+
+
+}
---
+++ src/main/java/com/procesos/concesionario/services/UserService.java
@@ -0,0 +1,12 @@
+package com.procesos.concesionario.services;
+
+import com.procesos.concesionario.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    User getUserById(Long Id);
+    List<User> allUser();
+    User createUser(User user);
+    User updateUser(Long id, User user);
+}
